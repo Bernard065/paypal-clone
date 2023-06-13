4019e5b1d53bcf8afd3c4cf66cc4c1ead3f895ba
+ <+>
2023-06-13 11:37:00 +0300

    creating PayOnline pages

--- src/pages/  PayOnline.js
+++ src/pages/  PayOnline.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Hero from './PayOnline/Hero'
+import Speed from './PayOnline/Speed'
+import ImageGrids from './PayOnline/ImageGrids'
+import PayOnlineSection from './PayOnline/PayOnlineSection'
+import Signup from '../components/HomePage/Signup'
 
 const   PayOnline = () => {
   return (
-    <div>  PayOnline</div>
+    <div>
+      <Hero />
+      <Speed />
+      <ImageGrids />
+      <PayOnlineSection />
+      <Signup />
+    </div>
   )
 }
 
